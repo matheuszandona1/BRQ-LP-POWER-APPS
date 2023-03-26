3f5aa4ccf01f815d4004a332e64150ada7483191
matheus <matheus@fermen.to>
2023-03-26 14:51:26 -0300

    🚧 v1

--- .dev/src/js/index.js
+++ .dev/src/js/index.js
@@ -2,15 +2,30 @@
 		$("#telefone").mask("(00) 00000-0000")
 	})
 
+	$(document).ready(function() {
+		$('.slide-parent').hide(); 
+		$('.slide-parent:first').show(); 
+		
+		$('.tablinks:first').addClass('active');
+	  
+		$('.slider__pills').click(function(){  
+		  var tab_id = $(this).attr('data-tab'); 
+		  $('.slider__pills').removeClass('active');  
+		  $('.slide-parent').hide(); 
+		  $('#'+tab_id).show(); 
+		  $(this).addClass('active');  
+
+		})
+	  })
 
 
-
-    $(".slide-parent").slick({
+	  $(".slide-parent").slick({
 		centerMode: true,
 		slidesToShow: 1,
 		centerPadding: "25%",
 		arrows:false,
 		dots:true,
+		autoplay: false,
 		responsive: [
 			{
 				breakpoint: 768,
@@ -32,19 +47,18 @@
 			},
 		],
 	})
-
-
-
+	
 	$(document).ready(function() {
-		$('.slide-parent').hide(); 
-		$('.slide-parent:first').show(); 
-		$('.tablinks:first').addClass('active');
+		$('.tab').click(function() {
+		  var tab_id = $(this).attr('data-switcher');
 	  
-		$('.slider__pills').click(function(){  
-		  var tab_id = $(this).attr('data-tab'); 
-		  $('.slider__pills').removeClass('active');  
-		  $('.slide-parent').hide(); 
-		  $('#'+tab_id).show(); 
-		  $(this).addClass('active');  
-		})
-	  })
+		  $('.tab').removeClass('active');
+		  $('.solutions__tabs--content').removeClass('active');
+	  
+		  $(".solutions__tabs--content").addClass('active');
+		  $(tab_id).addClass('active');
+	  
+		  return false;
+		});
+	  });
+	  
