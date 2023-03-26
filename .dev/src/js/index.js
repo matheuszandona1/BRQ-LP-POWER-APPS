35eb50445df502323cec622c1c124c96b0aaae58
matheus <matheus@fermen.to>
2023-03-26 18:54:34 -0300

    🚀 v1 BRQ

--- .dev/src/js/index.js
+++ .dev/src/js/index.js
@@ -1,64 +1,61 @@
-	$(document).ready(function () {
-		$("#telefone").mask("(00) 00000-0000")
+$(document).ready(function () {
+	$("#telefone").mask("(00) 00000-0000")
+})
+
+$(document).ready(function () {
+	$(".slide-parent").hide()
+	$(".slide-parent:first").show()
+
+	$(".slider__pills:first").addClass("active")
+
+	$(".slider__pills").click(function () {
+		var tab_id = $(this).attr("data-tab")
+		$(".slider__pills").removeClass("active")
+		$("#" + tab_id).show()
+		$(this).addClass("active")
 	})
+})
 
-	$(document).ready(function() {
-		$('.slide-parent').hide(); 
-		$('.slide-parent:first').show(); 
-		
-		$('.tablinks:first').addClass('active');
-	  
-		$('.slider__pills').click(function(){  
-		  var tab_id = $(this).attr('data-tab'); 
-		  $('.slider__pills').removeClass('active');  
-		  $('.slide-parent').hide(); 
-		  $('#'+tab_id).show(); 
-		  $(this).addClass('active');  
-
-		})
-	  })
-
-
-	  $(".slide-parent").slick({
-		centerMode: true,
-		slidesToShow: 1,
-		centerPadding: "25%",
-		arrows:false,
-		dots:true,
-		autoplay: false,
-		responsive: [
-			{
-				breakpoint: 768,
-				settings: {
-					arrows: false,
-					centerMode: true,
-					centerPadding: "40px",
-					slidesToShow: 1,
-				},
+$(".slide-parent").slick({
+	centerMode: true,
+	slidesToShow: 1,
+	centerPadding: "25%",
+	arrows: false,
+	dots: true,
+	autoplay: false,
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: "0px",
+				slidesToShow: 1,
 			},
-			{
-				breakpoint: 480,
-				settings: {
-					arrows: false,
-					centerMode: true,
-					centerPadding: "40px",
-					slidesToShow: 1,
-				},
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: "0px",
+				slidesToShow: 1,
 			},
-		],
+		},
+	],
+})
+$(document).ready(function () {
+	// switch tabs
+	$(".tab").click(function () {
+		var cont = $(this).data("switch")
+		$(".solutions__tabs--content").removeClass("active")
+		$("#" + cont).addClass("active")
+		$(".tab").removeClass("active")
+		$(this).addClass("active")
 	})
-	
-	$(document).ready(function() {
-		$('.tab').click(function() {
-		  var tab_id = $(this).attr('data-switcher');
-	  
-		  $('.tab').removeClass('active');
-		  $('.solutions__tabs--content').removeClass('active');
-	  
-		  $(".solutions__tabs--content").addClass('active');
-		  $(tab_id).addClass('active');
-	  
-		  return false;
-		});
-	  });
-	  
+})
+
+$(".faq__question").click(function () {
+	$(this).find(".faq__question--text").toggleClass("active")
+	$(this).find(".faq__question--answer").slideToggle()
+})
