5fa1c255c2b0d68b2505297e1575f4162a0378e8
diaslol <mastronelli88@gmail.com>
2023-05-10 16:42:27 -0300

    🚀 v0

--- .dev/src/js/index.js
+++ .dev/src/js/index.js
@@ -3,18 +3,17 @@ $(document).ready(function () {
 })
 
 $(".slide-parent").slick({
-	centerMode: true,
-	slidesToShow: 1,
-	centerPadding: "25%",
-	arrows: false,
+	slidesToShow: 3,
+	arrows: true,
 	dots: true,
 	autoplay: false,
+	// centerMode: true,
+	// centerPadding: "20%",
 	responsive: [
 		{
 			breakpoint: 768,
 			settings: {
 				arrows: false,
-				centerMode: true,
 				centerPadding: "0px",
 				slidesToShow: 1,
 			},
@@ -23,53 +22,13 @@ $(".slide-parent").slick({
 			breakpoint: 480,
 			settings: {
 				arrows: false,
-				centerMode: true,
 				centerPadding: "0px",
 				slidesToShow: 1,
 			},
 		},
 	],
 })
-$(document).ready(function () {
-	// ocultar todas as tabs, exceto a primeira
-	$(".slide-parent:not(:first)").hide()
 
-	// adicionar a classe "active" à primeira slider__pills
-	$(".slider__pills:first").addClass("active")
-
-	// quando uma slider__pills é clicada
-	$(".slider__pills").click(function () {
-		// obter o data-tab da slider__pills clicada
-		var tab_id = $(this).attr("data-tab")
-
-		// remover a classe "active" de todas as slider__pills
-		$(".slider__pills").removeClass("active")
-
-		// adicionar a classe "active" à slider__pills clicada
-		$(this).addClass("active")
-
-		// ocultar todas as tabs
-		$(".slide-parent").hide()
-		$(".slide-parent").slick("refresh")
-		console.log("passei por aq")
-
-		// mostrar a tab correspondente ao data-tab da slider__pills clicada
-		$("#" + tab_id).show()
-	})
-})
-$(document).ready(function () {
-	$(".w-tab-link").on("click", function () {})
-})
-$(document).ready(function () {
-	// switch tabs
-	$(".tab").click(function () {
-		var cont = $(this).data("switch")
-		$(".solutions__tabs--content").removeClass("active")
-		$("#" + cont).addClass("active")
-		$(".tab").removeClass("active")
-		$(this).addClass("active")
-	})
-})
 
 $(".faq__question").click(function () {
 	$(this).find(".faq__question--text").toggleClass("active")
